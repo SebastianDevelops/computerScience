46ea77cd4f403ebfa5412bbacb72980389f778a1
SebastianDevelops <sebastiandevelops@gmail.com>
2024-01-12 07:53:24 +0200

    hello world in java

---
+++ LearningJava/MyFirstProject/Main/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.print("Hello, World!\n");
+        System.out.println("My first Java Project!");
+    }
+}
