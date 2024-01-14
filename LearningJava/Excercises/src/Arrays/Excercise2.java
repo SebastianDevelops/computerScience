79661b2c93cc191f31076d216c84fc00d87c0280
SebastianDevelops <sebastiandevelops@gmail.com>
2024-01-14 14:11:40 +0200

    Excercise2 Arrays

---
+++ LearningJava/Excercises/src/Arrays/Excercise2.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Arrays;
+
+import Arrays.Excercise1;
+
+public class Excercise2 {
+    public static void main(String[] args) {
+        //Q: Reverse this array
+        int[] arr = { 10, 15, 29, 100, 8, 5 };
+
+        reverse(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void reverse(int[] arr)
+    {
+        //Two pointer
+        int startIndex = 0;
+        int endIndex = arr.length - 1;
+        
+        while (startIndex < endIndex) {
+            Excercise1.swap(arr, startIndex, endIndex);
+            startIndex++;
+            endIndex--;
+        }
+    }
+}
